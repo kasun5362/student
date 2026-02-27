8154a1fae57ff5e912207c13c75509fe8c8ac695
kasun5362 <nethminakasun3@gmail.com>
2026-02-28 02:10:00 +0530

    update and delete done

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,9 @@ Route::get('/', function () {
 Route::get('/addStudent', [StudentController::class,"showAddStudent"]);
 Route::post("/postStudent", [StudentController::class,"postStudent"]);
 Route::get("/student",[StudentController::class,"index"]);
+Route::delete("/deleteStudent/{id}",[StudentController::class,"deleteStudent"]);
+
+Route::get("/updateStudent/{id}",[StudentController::class,"showUpdateStudent"]);
+
+Route::put("/putUpdateStudent/{id}",[StudentController::class,"putupdateStudent"]);
 
